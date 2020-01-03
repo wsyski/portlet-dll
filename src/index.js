ad1c386c1ba0d647a7d1defd3d01e7839c8c8429
wsyski <wsyski@gmail.com>
2020-01-03 21:09:38 +0100

    Initial version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const portletDll = require("./portlet-dll/portlet-dll.js");
-var _ADAPT_RT_ = require("./adapt-rt");
 
 (function (exports) {
-  exports.portletDll = portletDll(null, _ADAPT_RT_);
+  exports.portletDll = portletDll();
 }(window.Arena = window.Arena || {}));
 
 
