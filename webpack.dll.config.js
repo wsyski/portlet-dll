a455e833d20aacefef8fa2d7576f3eb26b70a9f1
wsyski <wsyski@gmail.com>
2020-01-11 00:15:59 +0100

    Initial version

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -4,6 +4,10 @@ const pkg = require("./package.json");
 
 module.exports = {
   mode: "development",
+  context: process.cwd(),
+  resolve: {
+    modules: [__dirname, 'node_modules']
+  },
   optimization: {
     minimize: false,
     noEmitOnErrors: true
@@ -17,8 +21,7 @@ module.exports = {
       "@angular/platform-browser",
       "@angular/platform-browser-dynamic",
       "@angular/router",
-      "rxjs",
-      "tslib",
+      'rxjs',
       "zone.js/dist/zone"
     ]
   },
@@ -30,8 +33,12 @@ module.exports = {
   plugins: [
     new webpack.DllPlugin({
       path: path.join("dll", pkg.name, "[name]-dll-manifest.json"),
-      entryOnly: true,
-      name: "[name]"
-    })
+      name: "[name]",
+      entryOnly: true
+    }),
+    new webpack.ContextReplacementPlugin(
+      /\@angular(\\|\/)core(\\|\/)fesm5/,
+      path.join(__dirname, './src')
+    )
   ]
 };
